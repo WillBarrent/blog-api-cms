bd9e9064e688da3d8db1fab8512ef667ef1122a4
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-04 15:30:40 +0500

    Implement routing with react-router

---
+++ routes.jsx
@@ -0,0 +1,14 @@
+import App from "./src/App";
+
+const routes = [
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/login",
+        element: <App />
+    },
+];
+
+export default routes;
