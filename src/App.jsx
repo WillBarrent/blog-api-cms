6c06335ad5b25d763837315f41b8c2d3efde9662
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-05 19:40:36 +0500

    Make sure if author is not logged in to navigate him to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useLocation, useNavigate } from 'react-router';
 import './App.css';
 
 import Header from "./Components/Header/Header";
 import Dashboard from './Pages/Dashboard/Dashboard';
+import Login from './Pages/Login/Login';
+import { useEffect } from 'react';
+import BlogCreate from './Pages/BlogCreate/BlogCreate';
 
 function App() {
+  const location = useLocation();
+  const locationPathName = location.pathname;
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, []);
+
+
   return (
     <>
       <Header />
-      <Dashboard />
+      {(locationPathName === '/login') ? <Login /> : (locationPathName === '/create-blog') ? <BlogCreate /> : <Dashboard />}
     </>
   );
 }
