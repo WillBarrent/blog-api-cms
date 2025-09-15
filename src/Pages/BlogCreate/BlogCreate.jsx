7307b056aedd60204dbcc0cf25c45a9f1e02cead
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-09-15 17:02:30 +0500

    Remove unnecessary operations before creating post

--- src/Pages/BlogCreate/BlogCreate.jsx
+++ src/Pages/BlogCreate/BlogCreate.jsx
@@ -11,19 +11,7 @@ function BlogCreate() {
     const navigate = useNavigate();
 
     async function getHtmlContent() {
-        const content = editorRef.current.getContent().split("\n").join('');
-
-        const beforeCodeTagIndexes = Array.from(content.matchAll("<code>")).map((array) => array.index + 6);
-        const afterCodeTagIndexes = Array.from(content.matchAll("</pre>")).map((array) => array.index - 7);
-
-        let replacedContent = null;
-
-        for (let i = 0; i < beforeCodeTagIndexes.length; i++) {
-            const code = content.substring(beforeCodeTagIndexes[i], afterCodeTagIndexes[i]);
-            const highlightedCode = Prism.highlight(code, Prism.languages.js, "js");
-
-            replacedContent = content.replaceAll(content.substring(beforeCodeTagIndexes[i] - 6, afterCodeTagIndexes[i] + 7), highlightedCode);
-        }
+        let content = editorRef.current.getContent();
 
         await fetch("http://localhost:3000/api/posts", {
             method: "POST",
@@ -32,7 +20,7 @@ function BlogCreate() {
                 "Authorization": localStorage.getItem('token'),
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ title: title, "content": (replacedContent === null) ? content : replacedContent }),
+            body: JSON.stringify({ title: title, content: content }),
         });
 
         navigate("/");
