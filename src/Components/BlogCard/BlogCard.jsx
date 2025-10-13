7a8682a692b7873b30997b81843989596930729b
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-10-13 19:14:06 +0500

    Change stylization of blog card and add delete functionality

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,17 +1,35 @@
-import styles from './BlogCard.module.css';
+import { Link } from "react-router";
+import styles from "./BlogCard.module.css";
 
-function BlogCard() {
-    return <div className={styles.blogCard}>
-        <div className={styles.blogCardInfo}>
-            <div className={styles.blogCardTitle}>Post title</div>
-            <div className={styles.blogCardCreatedAt}>11/09/2025</div>
-        </div>
+function BlogCard({ title, createdAt, postId }) {
+  async function deletePost() {
+    await fetch("http://localhost:3000/api/posts/" + postId, {
+      method: "DELETE",
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+  }
+
+  return (
+    <div className={styles.blogCard}>
+      <Link to={"/posts/" + postId}>
+        <p className={styles.blogCardTitle}>{title}</p>
+      </Link>
+      <div className={styles.blogCardInfo}>
+        <div className={styles.blogCardCreatedAt}>{createdAt}</div>
         <div className={styles.blogCardActions}>
-            <div className={styles.blogCardDelete}>Delete</div>
-            <div className={styles.blogCardEdit}>Edit</div>
-            <div className={styles.blogCardPublish}>Publish</div>
+          <button onClick={deletePost} className={styles.blogCardDelete}>
+            Delete
+          </button>
+          <Link to={"/update-blog/" + postId}>
+            <button className={styles.blogCardEdit}>Edit</button>
+          </Link>
+          <button className={styles.blogCardPublish}>Publish</button>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default BlogCard;
